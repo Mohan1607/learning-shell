4a3805ed80a031fa5c2900c5f98c733d8e56ae63
mohan <venkyravipati99@gmail.com>
2022-11-23 11:30:20 +0530

    cart script

---
+++ 02-vars.sh
@@ -0,0 +1,12 @@
+# variables- variables are nothing but, if we assign name to set of data is called variable
+# VARNAME=DATA
+a=10
+# Acess the variable -$VARNAME
+echo A- $a
+# a=10 is hardcoded manually however we need the data dynamically
+# dynamically you can get in 2 ways
+# 1.user input
+# 2.Script find with other commands
+# command substitution
+# VARNAME=$(command)
+# output of the command stored in variable
