aa8fac9949f7cbe5ef0505b558d5716f26aebc77
mohan <venkyravipati99@gmail.com>
2022-11-25 18:22:04 +0530

    print script

--- 01-print.sh
+++ 01-print.sh
@@ -3,3 +3,8 @@
 #find / -name shadow
 
 echo -e "\e[31mSUCCESS\e[0m"
+# -e -> to enable \e
+# \e -> to enable color
+# 31 -> colour code  31 -> RED 32-> GREEN 33->YELLOW 34-> BLUE 35-> Magenta 36-> CYAN
+# 0m -> Disable the enable colour
+
