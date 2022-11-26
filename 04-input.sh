1d29223231962c9885cfc47129dc0bee46a0731d
mohan <venkyravipati99@gmail.com>
2022-11-26 21:39:46 +0530

    input script

--- 04-input.sh
+++ 04-input.sh
@@ -1,6 +1,6 @@
-#read -p "enter mysql user name =" user
+read -p "enter mysql user name =" user
 read -s -p "enter mysql password =" pass
-echo username and password from read = $pass$user
+echo username and password from read = $pass $user
 #echo "username & password from read = $user&$pass"
 
 
