87777083692b443f5125269ef32a5cd53c5da614
mohan <venkyravipati99@gmail.com>
2022-11-27 11:03:18 +0530

    function script

--- 05-functions.sh
+++ 05-functions.sh
@@ -1,12 +1,15 @@
 sample () {
   #echo I AM IN FUNCTION
-  echo A=$a
-  b=$2
+  #echo A=$a
+  #b=20
+  echo first value=$1
+  echo econd value=$2
 }
 
 ##### main program
-a=$1
-sample
-echo B=$b
+#a=10
+#sample
+#echo B=$b
+sample 10 20
 
 
